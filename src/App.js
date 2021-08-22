bff25fd1506f4e46e9b6829e56da33e14a42aef5
FrederickMih <fredtimermih@gmail.com>
2021-08-22 05:05:13 +0100

    Delete any unnecessary file or asset

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      
-          Learn React
-       
-      </header>
+      <header className="App-header">BookStore</header>
     </div>
   );
 }
