367f606f65b793a43dde6e7e3603eb27513f35d5
FrederickMih <fredtimermih@gmail.com>
2021-08-22 17:15:40 +0100

    Add BooksList table header

---
+++ src/containers/BooksList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BooksList = () => (
+  <div>
+    <table>
+      <thead>
+        <tr>Book ID</tr>
+        <tr>Title</tr>
+        <tr>Category</tr>
+      </thead>
+    </table>
+  </div>
+);
+
+export default BooksList;
