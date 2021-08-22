e43e5c84922971488b47006c881ea2e886c8309f
FrederickMih <fredtimermih@gmail.com>
2021-08-22 04:40:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      
+          Learn React
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
