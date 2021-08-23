aee4319ded6e3d3e26e64fb0a057e13a0de5b6bf
FrederickMih <fredtimermih@gmail.com>
2021-08-23 09:33:36 +0100

    Add table body

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const BooksList = () => (
-  <div>
-    <table>
-      <thead>
-        <tr>Book ID</tr>
-        <tr>Title</tr>
-        <tr>Category</tr>
-      </thead>
-    </table>
-  </div>
-);
+const BooksList = () => {
+  const books = useSelector((state) => state);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>Book ID</td>
+            <td>Title</td>
+            <td>Category</td>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map((mybook) => (
+            <tr key={mybook.id}>
+              <td>{mybook.id}</td>
+              <td>{mybook.title}</td>
+              <td>{mybook.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default BooksList;
