d0f3373f14d3d1df93ce4731d9b0ecdd177aabd1
FrederickMih <fredtimermih@gmail.com>
2021-08-23 13:04:57 +0100

    Add initial books with title and categories

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import booksReducers from './reducers/books';
 import App from './components/App';
+
+const initState = [
+  {
+    id: new Date().getTime().toString(),
+    title: 'titleOne',
+    category: 'Sci-Fi',
+  },
+  {
+    id: new Date().getTime().toString(),
+    title: 'titleTwo',
+    category: 'Action',
+  },
+  {
+    id: new Date().getTime().toString(),
+    title: 'titleThree',
+    category: 'Biography',
+  },
+  {
+    id: new Date().getTime().toString(),
+    title: 'titleFour',
+    category: 'Kids'
+  }
+];
+
+
 
 ReactDOM.render(
   <React.StrictMode>
