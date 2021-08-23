a5e6ef6aa092d4523d5c2e60d82d916f0cfcc875
FrederickMih <fredtimermih@gmail.com>
2021-08-23 10:37:57 +0100

    Add select field for books categories

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,15 +1,26 @@
-import Reader from 'react';
+import React from 'react';
 
 const BooksForm = () => {
+  const categories = [
+    'Mathematics',
+    'Physics',
+    'Chemistry',
+    'History',
+    'English'
+  ];
 
-   return (
-      <>
+  return (
+    <>
       <form action="">
-         <input type="text" id="title" placeholder = 'title here' />
+        <input type="text" id="title" placeholder="title here" />
+        <select name="category">
+          {categories.forEach((item) => (
+            <option>{item}</option>
+          ))}
+        </select>
       </form>
-      </>
-   );
-
-}
+    </>
+  );
+};
 
 export default BooksForm;
