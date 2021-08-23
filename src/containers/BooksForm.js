7b8eb4f38496acc45eb16aa2962dd610b4104105
FrederickMih <fredtimermih@gmail.com>
2021-08-23 10:16:55 +0100

    Add input form for book title

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,15 @@
+import Reader from 'react';
+
+const BooksForm = () => {
+
+   return (
+      <>
+      <form action="">
+         <input type="text" id="title" placeholder = 'title here' />
+      </form>
+      </>
+   );
+
+}
+
+export default BooksForm;
