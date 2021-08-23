121008bf3b7ab2b088de33f52c76b4ce6bd4c0e9
FrederickMih <fredtimermih@gmail.com>
2021-08-23 10:05:17 +0100

    Add Book's propTypes

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Book = ({ book }) => {
   const { id, title, category } = book;
@@ -12,3 +13,12 @@ const Book = ({ book }) => {
     </div>
   );
 };
+
+Book.propTypes = {
+  book: PropTypes.InstanceOf(Object).isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired
+};
+
+export default Book;
