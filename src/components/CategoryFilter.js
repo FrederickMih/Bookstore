c45b9ab948f2313d1080554ad667f101a44cc1ee
FrederickMih <fredtimermih@gmail.com>
2021-08-26 17:46:44 +0100

    Pass handleFilterChange to Category component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,27 +1,31 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import * as myConst from '../constants';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const CategoryFilter = () => {
-  const [filter, setFilter] = useState('All');
-  const dispatch = useDispatch();
-
-  const categories = ['All', 'Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
-    const filter = e.target.value;
-    dispatch({ type: myConst.CHANGE_FILTER, filter });
-  };
+const CategoryFilter = ({ handleFilterChange }) => {
+  const categories = [
+    'All',
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
 
   return (
-    <div>
-      <select onChange={(e) => handleFilterChange(e)} value={filter}>
-        {categories.map((catItem) => <option key={Math.random() * 10}>{catItem}</option>)}
-
-      </select>
-    </div>
+    <select onChange={handleFilterChange} name="filter">
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
   );
+};
+
+CategoryFilter.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
 };
 
 export default CategoryFilter;
